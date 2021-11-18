f261843fa298ce74e82da963303d1fab6c676b7a
CoderYooda <coderyooda@gmail.com>
2021-11-18 17:53:46 +0300

    awd

--- app/src/main/java/com/yooda/app/StorePage.java
+++ app/src/main/java/com/yooda/app/StorePage.java
@@ -2,13 +2,34 @@ package com.yooda.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StorePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_store_page);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(StorePage.this,
+                        AuthActivity.class);
+                startActivity(i);
+            }
+        }, 3000);
+
     }
 }
